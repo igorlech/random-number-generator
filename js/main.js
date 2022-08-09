a424db6f68ab29337bd5e18bfc7c0abb0351bb9d
igorlech <eaaigle@students.eaaa.dk>
2022-08-09 22:34:37 +0200

    Update#1
    Creating if statement and first mechanics: checking if numbers are corect, getting numbers

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-let number1 = document.querySelector('#number1').value;
-let number2 = document.querySelector('#number2').value;
+let submit = document.querySelector('.submitbutton');
+let wrapper = document.querySelector('.gnrl-wrapper');
 let output = document.querySelector('.output');
 
-console.log(number1)
+wrapper.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const number1 = parseFloat(document.querySelector('#number1').value);
+    const number2 = parseFloat(document.querySelector('#number2').value);
+    if (number1 >= number2) {
+        output.innerHTML = "";
+        output.insertAdjacentHTML("beforeend", "The first number has to be lower then the second one.");
+    } else {
+        output.innerHTML = "";
+        output.insertAdjacentHTML("beforeend", "Works");
+    }
+})
