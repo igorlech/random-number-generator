7d182a812022eb97491e50aed8320f1ff9b59190
igorlech <eaaigle@students.eaaa.dk>
2022-08-09 13:20:08 +0200

    First progress
    Done: HTML, CSS, JS files started

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let number1 = document.querySelector('#number1').value;
+let number2 = document.querySelector('#number2').value;
+let output = document.querySelector('.output');
+
+console.log(number1)
