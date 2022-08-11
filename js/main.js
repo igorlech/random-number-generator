4b0c335fee4f47a780bde337fad1b46062b3da30
igorlech <eaaigle@students.eaaa.dk>
2022-08-11 15:20:03 +0200

    Final update
    Adding function that generates random numbers.

--- js/main.js
+++ js/main.js
@@ -6,11 +6,18 @@ wrapper.addEventListener("submit", function(e) {
     e.preventDefault();
     const number1 = parseFloat(document.querySelector('#number1').value);
     const number2 = parseFloat(document.querySelector('#number2').value);
+
+    function getRandomNumber (min, max) {
+        min = Math.ceil(number1);
+        max = Math.floor(number2);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
     if (number1 >= number2) {
         output.innerHTML = "";
         output.insertAdjacentHTML("beforeend", "The first number has to be lower then the second one.");
     } else {
         output.innerHTML = "";
-        output.insertAdjacentHTML("beforeend", "Works");
+        output.insertAdjacentHTML("beforeend", `Your number: ${getRandomNumber()}`);
     }
 })
